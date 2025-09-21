3011aaf66a4e34f98f13a2f72374580e5920377b
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-09-21 23:21:11 +0800

    Home UI

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,18 @@
+import { Link } from 'react-router-dom';
+import './App.css'
+
 const Home = () => {
     return (<>
-        <div>
-            
+        <div className="w-screen h-screen items-center flex flex-row p-1 container">
+            <div className=' w-1/2'>
+                <p className=' font-bold text-3xl'>STI ID</p>
+                <p className=''>Capture your best photo on your ID</p>
+                
+                <Link to="/ID" className='inline-block p-2 mt-5 bg-yellow-200 rounded-sm shadow-md hover:bg-blue-400 transition hover:text-white font-semibold'>LETS GO</Link>
+            </div>
+            <div className='w-1/2'>
+                <div className='bg-amber-600 p-10 rounded-full shadow-md'></div>
+            </div>
         </div>
     </> );
 }
