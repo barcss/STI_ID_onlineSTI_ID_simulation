f74d2b437617a7ffb6df879e1314e3503e6dab0c
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-09-22 23:54:47 +0800

    redesign home

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,14 +3,14 @@ import './App.css'
 
 const Home = () => {
     return (<>
-        <div className="w-screen h-screen items-center flex flex-row p-1 container">
+        <div className="w-screen h-screen items-center justify-center flex flex-row p-1 container ">
             <div className=' w-1/2'>
-                <p className=' font-bold text-3xl'>STI ID</p>
-                <p className=''>Capture your best photo on your ID</p>
+                <p className=' font-bold text-3xl'>ID Frame</p>
+                <p className=''>Capture the best photo on your ID</p>
                 
                 <Link to="/ID" className='inline-block p-2 mt-5 bg-yellow-200 rounded-sm shadow-md hover:bg-blue-400 transition hover:text-white font-semibold'>LETS GO</Link>
             </div>
-            <div className='w-1/2'>
+            <div className='md:w-1/2 hidden md:block'>
                 <div className='bg-amber-600 p-10 rounded-full shadow-md'></div>
             </div>
         </div>
