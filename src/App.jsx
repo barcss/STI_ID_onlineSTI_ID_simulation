426e2fc85ded3e5de74f812716d7e63ff1455c3d
John Paul Barcinilla <barcinillajohnpaul106@gmail.com>
2025-09-22 23:54:01 +0800

    add new route and bg color

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom"
 import Home from './Home'
+import IDModel from "./IDModel";
+import Test from "./test";
 
 const App = () => {
   return (<>
     <BrowserRouter>
+    <div className=" flex justify-center home-bg">
       <Routes>
         <Route path="/" element={<Home/>} />
+        <Route path="/ID" element={<IDModel/>} />
+        <Route path="/test" element={<Test/>} />
       </Routes >
+      </div>
     </BrowserRouter>
+    
   </>);
 }
  
